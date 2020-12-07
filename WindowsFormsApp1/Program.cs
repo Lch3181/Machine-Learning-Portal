266685fcc5ef8cfec94a5c5afc058c6288849998
Lch#3181 <34583022+Lch3181@users.noreply.github.com>
2020-12-06 20:16:24 -0500

    now can check dependency from cmd on start

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace WindowsFormsApp1
 {
@@ -14,9 +15,31 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            //Check dependency
+            MessageBox.Show(CheckDependency("/C python --version"));
+            MessageBox.Show(CheckDependency("/C pip --version"));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        static string CheckDependency(string input)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.Arguments = input;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            string output = cmd.StandardOutput.ReadToEnd();
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            return output;
         }
     }
 }
